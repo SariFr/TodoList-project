c2cbbffa6290450ee1aa5fa6fdabbb4cc2db24b6
Frayman <Frayman@DESKTOP-4U42C65>
2025-01-07 02:29:30 +0200

    update focker file

--- server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2737e0788cadac94f1c493f23610b19ef86dd69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8afb7153d31dc71792e4f70e92a0cd5295f8239f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
