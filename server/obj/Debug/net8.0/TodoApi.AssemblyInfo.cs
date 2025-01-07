8afb7153d31dc71792e4f70e92a0cd5295f8239f
Frayman <Frayman@DESKTOP-4U42C65>
2025-01-07 02:17:58 +0200

    add commit

--- server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1713fae7f1c1c64599be17f0cee5d3cb94bc8580")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2737e0788cadac94f1c493f23610b19ef86dd69")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
